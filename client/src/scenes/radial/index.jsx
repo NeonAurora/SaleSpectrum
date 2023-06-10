6fd1f36bc223529f8682544996a7249ccf891ef8
Arn <arnarnsde@gmail.com>
2023-06-11 00:10:32 +0600

    c1

--- client/src/scenes/radial/index.jsx
+++ client/src/scenes/radial/index.jsx
@@ -6,7 +6,7 @@ import RadialChart from "components/RadialChart";
 const Radial = () => {
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="BREAKDOWN" subtitle="Breakdown of Sales By Category" />
+      <Header title="Sales Impact" subtitle="Breakdown of Sales Impact By Admin Logs" />
       <Box mt="40px" height="75vh">
         <RadialChart />
       </Box>
