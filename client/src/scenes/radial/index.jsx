8aadd55e758e1702847de61b2995f2b828fdbc2b
Arn <arnarnsde@gmail.com>
2023-06-10 23:11:48 +0600

    Radial Chart Added

--- client/src/scenes/radial/index.jsx
+++ client/src/scenes/radial/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+import RadialChart from "components/RadialChart";
 
-const radial = () => {
+const Radial = () => {
   return (
-    <div>radial</div>
-  )
-}
+    <Box m="1.5rem 2.5rem">
+      <Header title="BREAKDOWN" subtitle="Breakdown of Sales By Category" />
+      <Box mt="40px" height="75vh">
+        <RadialChart />
+      </Box>
+    </Box>
+  );
+};
 
-export default radial
+export default Radial;
